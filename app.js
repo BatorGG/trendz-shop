2d271186fe1fd146461507352cd293aca7cc6020
Bátor Kárpáthegyi <bator.karpathegyi@gmail.com>
2022-11-08 14:01:00 +0100

    vercel.json 2

--- app.js
+++ app.js
@@ -434,3 +434,5 @@ app.post("/send-application", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
